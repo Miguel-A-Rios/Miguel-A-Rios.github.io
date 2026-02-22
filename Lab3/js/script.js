fbb969e1ce4ac96397d55d553a590de9fed72f72
Miguel Rios <miguelarios@student.hartnell.edu>
2026-02-21 21:03:37 -0800

    push lab3

--- Lab3/js/script.js
+++ Lab3/js/script.js
@@ -1,11 +1,13 @@
 // Event listeners
 document.querySelector("button").addEventListener("click", gradeQuiz);
 
+let attempts = localStorage.getItem("total_attempts")
+
 // Must call function so it can run
 shuffleQ1Choices();
 
 function shuffleQ1Choices() {
-    let q1Choices = ["font-color", "color", "text-color", "font-color"];
+    let q1Choices = ["treecko", "tornich", "mudkip", "charmander"];
     q1Choices = _.shuffle(q1Choices);
     console.log(q1Choices);
 
@@ -20,30 +22,34 @@ function shuffleQ1Choices() {
         
         labelElement.prepend(radioElement);
 
-        // document.querySelector("#q1ChoicesDiv").append(labelElement);
+        document.querySelector("#q1Choices").append(labelElement);
         
         console.log(labelElement);
     } //for
     
 }
 
-let final_score = 0;
+let final_score;
 
 function gradeQuiz() {
+    final_score = 0;
     let answerQ1 = "tornich";
     let userAnswer1 = document.querySelector("input[name=q1]:checked").value;
 
-    alert("Grading quiz..." + userAnswer1);
+    // alert("Grading quiz..." + userAnswer1);
 
     if (userAnswer1 == answerQ1) {
         // Display "Right!"
         document.querySelector("#q1Answer").textContent = "Right!";
         document.querySelector("#q1Answer").style.color = "green";
+        document.querySelector("#q1AnswerImg").innerHTML = "<img src='img/green-checkmark.jpeg' width='40' height='40'>";
         final_score += 20;
     }
     else {
         document.querySelector("#q1Answer").textContent = "Wrong!";
         document.querySelector("#q1Answer").style.color = "red";
+        document.querySelector("#q1AnswerImg").innerHTML = "<img src='img/red-x.jpeg' width='40' height='40'>";
+
     }
 
     gradeQ2();
@@ -52,6 +58,11 @@ function gradeQuiz() {
     gradeQ5();
 
     document.querySelector("#finalScore").textContent = "Final Score: " + final_score;
+
+    attempts++;
+    localStorage.setItem("total_attempts", attempts);
+
+    document.querySelector("#totalAttempts").textContent = "Total Attempts: " + attempts;
 
     if(final_score >= 80){
         document.querySelector("#congratMsg").textContent = "Congratulations you scored above 80!";
@@ -65,11 +76,13 @@ function gradeQ2() {
     if (choice.value === "unova") {
         document.querySelector("#q2Answer").textContent = "Right!";
         document.querySelector("#q2Answer").style.color = "green";
+        document.querySelector("#q2AnswerImg").innerHTML = "<img src='img/green-checkmark.jpeg' width='40' height='40'>";
         final_score += 20;
-        alert("Grading quiz..." + choice.value);
+        // alert("Grading quiz..." + choice.value);
     } else {
         document.querySelector("#q2Answer").textContent = "Wrong!";
         document.querySelector("#q2Answer").style.color = "red";
+        document.querySelector("#q2AnswerImg").innerHTML = "<img src='img/red-x.jpeg' width='40' height='40'>";
     }
 }
 
@@ -79,6 +92,8 @@ function gradeQ3() {
     if(ans_element == "Flying"){
         document.querySelector("#q3Answer").textContent = "Right!";
         document.querySelector("#q3Answer").style.color = "green";
+        document.querySelector("#q3AnswerImg").innerHTML = "<img src='img/green-checkmark.jpeg' width='40' height='40'>";
+
         final_score += 20;
     }
     else {
@@ -92,6 +107,7 @@ function gradeQ4(){
     if(answerq4 == 2){
         document.querySelector("#q4Answer").textContent = "Right!";
         document.querySelector("#q4Answer").style.color = "green";
+        document.querySelector("#q4AnswerImg").innerHTML = "<img src='img/green-checkmark.jpeg' width='40' height='40'>";
         final_score += 20;
     } else {
         document.querySelector("#q4Answer").textContent = "Wrong!";
@@ -100,11 +116,14 @@ function gradeQ4(){
 }
 
 function gradeQ5() {
-    let choice = document.querySelector("#q5");
+    // let choice = document.querySelector("#q5");
+    let choice = document.querySelector("input[name='q5']:checked");
+
     if (choice.value == "Ground") {
         document.querySelector("#q5Answer").textContent = "Right!";
         document.querySelector("#q5Answer").style.color = "green";
-        alert("Grading quiz..." + choice.value);
+        document.querySelector("#q5AnswerImg").innerHTML = "<img src='img/green-checkmark.jpeg' width='40' height='40'>";
+        // alert("Grading quiz..." + choice.value);
         final_score += 20;
     } else {
         document.querySelector("#q5Answer").textContent = "Wrong!";
