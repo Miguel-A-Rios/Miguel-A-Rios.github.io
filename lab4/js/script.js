2fba3d35e073e40ddfe91c6284fabeda6924c6c1
Miguel Rios <miguelarios@student.hartnell.edu>
2026-02-28 10:53:49 -0800

    push lab4

--- lab4/js/script.js
+++ lab4/js/script.js
@@ -12,10 +12,15 @@ passwordElement.addEventListener("click", displaySuggestedPassword);
 usernameElement.addEventListener("change", checkUsername);
 
 
+let stateElement = document.querySelector("#state").addEventListener("change", displayCounties);
+
+document.querySelector("#passwordCheck").addEventListener("change", checkPassword);
+
+document.querySelector("#passwordMatch").addEventListener("change", checkPassword);
+
 displayStates();
 displaySuggestedPassword();
-// checkUsername();
-// displayLatitude();
+
 async function displayStates() {
     let url = "https://csumb.space/api/allStatesAPI.php";
     try {
@@ -33,7 +38,6 @@ async function displayStates() {
             optionEl.value = i.usps;
 
             document.querySelector("#state").append(optionEl);
-
         }
 
     } catch (err) {
@@ -53,6 +57,12 @@ async function displayCity() {
     let data = await response.json();
     console.log(data);
     //alert(data.city);
+    if (!data || !data.city) {
+        document.querySelector("#city").textContent = "Zip code not found";
+        document.querySelector("#latitude").textContent = "";
+        document.querySelector("#longitude").textContent = "";
+        return;
+    }
     document.querySelector("#city").textContent = data.city;
     document.querySelector("#latitude").textContent = data.latitude;
     document.querySelector("#longitude").textContent = data.longitude;
@@ -77,13 +87,64 @@ async function checkUsername() {
     let data = await response.json();
     console.log(data);
 
-    // document.querySelector("#usernameCheck").textContent = data.username;
+    if (usernameCheck.length < 3) {
+        document.querySelector("#usernameStatus").textContent = "";
 
-    if (data.available) {
+        document.querySelector("#usernameLength").textContent = "Username needs at least 3 characters.";
+        document.querySelector("#usernameLength").style.color = "red";
+    } else if (data.available) {
+        document.querySelector("#usernameLength").textContent = "";
+
         document.querySelector("#usernameStatus").textContent = "Username is available!";
         document.querySelector("#usernameStatus").style.color = "green";
     } else {
+        document.querySelector("#usernameLength").textContent = "";
+
         document.querySelector("#usernameStatus").textContent = "Username is NOT available.";
         document.querySelector("#usernameStatus").style.color = "red";
+    }
+}
+
+async function displayCounties() {
+    let state = document.querySelector("#state").value;
+
+    // clears the county list when changing to new state option
+    let countySelect = document.querySelector("#county");
+    countySelect.innerHTML = "";
+
+
+    let url = "https://csumb.space/api/countyListAPI.php?state=" + state;
+    let response = await fetch(url);
+
+    let data = await response.json();
+    console.log(data);
+    //alert(data[0].state); 
+
+    for (let i of data) {
+        let optionEl = document.createElement("option");
+        optionEl.textContent = i.county;
+        optionEl.value = i.usps;
+
+        document.querySelector("#county").append(optionEl);
+
+    }
+}
+document.querySelector("#passwordMatchStatus").textContent = "";
+
+
+function checkPassword() {
+    let passwordValue = document.querySelector("#passwordCheck").value;
+    let retypePasswordValue = document.querySelector("#passwordMatch").value;
+
+    document.querySelector("#passwordMatchStatus").textContent = "";
+    document.querySelector("#passwordLength").textContent = "";
+
+    if (passwordValue.length < 6) {
+        document.querySelector("#passwordLength").textContent = "Password must have at least 6 characters.";
+        document.querySelector("#passwordLength").style.color = "red";
+    }
+    if (passwordValue != retypePasswordValue) {
+        document.querySelector("#passwordMatchStatus").textContent = "Passwords don't match.";
+        document.querySelector("#passwordMatchStatus").style.color = "red";
     }
 }
